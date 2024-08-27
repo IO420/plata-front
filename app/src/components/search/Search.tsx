c3f3450cd18e04e87f6ff83808266bda28d7017c
IO <IO@IO.com>
2024-08-27 15:58:43 -0600

    new mapeo and fixed search

--- app/src/components/search/Search.tsx
+++ app/src/components/search/Search.tsx
@@ -3,35 +3,32 @@ import React, { useState } from "react";
 import Kind from "../kind/Kind";
 import "./search.css";
 import Mapeo from "../Mapeo/Mapeo";
+import { useFetch } from '../../services/productService';
 
 export default function Search() {
   const [filters, setFilters] = useState({});
+  const { data: kinds, error } = useFetch("/kind");
 
   const handleKindClick = (kind: string) => {
-    console.log({kind})
-    setFilters({
-      kind,
-    });
+    console.log({kind});
+    setFilters({ kind });
   };
+
+  if (error) {
+    return <div>Error loading data</div>;
+  }
 
   return (
     <>
       <div className="searchContainer">
-        <Kind
-          img="https://via.placeholder.com/150"
-          title="Anillos"
-          onClick={handleKindClick}
-        />
-        <Kind
-          img="https://via.placeholder.com/150"
-          title="Collares"
-          onClick={handleKindClick}
-        />
-        <Kind
-          img="https://via.placeholder.com/150"
-          title="Pulceras"
-          onClick={handleKindClick}
-        />
+        {kinds.map((kind: any) => (
+          <Kind
+            key={kind.id_kind}
+            img={kind.url}
+            title={kind.name}
+            onClick={handleKindClick}
+          />
+        ))}
       </div>
       <Mapeo filters={filters} />
     </>
